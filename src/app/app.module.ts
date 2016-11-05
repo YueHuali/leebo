2fb86201dac90308f54b31918e4204e18a7d2e5c
jun-ren <junren@qingyuanos.com>
2016-11-05 18:26:01 +0800

    Add route for dashboard, project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from "@angular/router";
+
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProjectComponent } from './project/project.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +20,22 @@ import { ProjectComponent } from './project/project.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'project',
+        component: ProjectComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
