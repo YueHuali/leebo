a57a91f0d0338fa2d0e53a39a58876478292b290
plfan <plfan@qingyuanos.com>
2016-11-09 14:24:52 +0800

    style(view): add project icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { DashboardComponent }  from './dashboard/dashboard.component';
 import { ProjectComponent }  from './project/project.component';
+import {DeleteComponent} from "./project/delete/delete.component";
 
 @NgModule({
   declarations: [
