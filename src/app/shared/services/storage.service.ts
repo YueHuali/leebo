01944dc5b82c9ef3aaee3d75699396928e08956f
yuehl <hlyue@qingyuanos.com>
2017-01-17 14:27:34 +0800

    Add new feature for storage pv.

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -13,6 +13,58 @@ export class StorageService {
     return this.http.get(BASE_OC_URI + '/api/v1/persistentvolumes');
   }
 
+  getStorageByName(name: string): Observable<any> {
+    return this.http.get(BASE_OC_URI + '/api/v1/persistentvolumes/' + name);
+  }
+
+  createPV(name: string, size: number, export_locations: string) {
+    let addrArr = export_locations.split(':');
+    let ip = addrArr[0];
+    let path = addrArr[1];
+
+    let url = BASE_OC_URI + '/api/v1/persistentvolumes';
+    let body = {
+      "kind": "PersistentVolume",
+      "apiVersion": "v1",
+      "metadata": {
+        "name": name,
+        "creationTimestamp": null
+      },
+      "spec": {
+        "capacity": {
+          "storage": size + "Gi"
+        },
+        "nfs": {
+          "server": ip,
+          "path": path
+        },
+        "accessModes": [
+          "ReadWriteMany"
+        ],
+        "persistentVolumeReclaimPolicy": "Recycle"
+      },
+      "status": {
+        "phase": "Pending"
+      }
+    };
+    console.log('url=', url);
+    console.log('body=', JSON.stringify(body));
+    return this.http.post(url, body).subscribe();
+
+  }
+
+  deletePV(name: string) {
+    let url = BASE_OC_URI + '/api/v1/persistentvolumes';
+    console.log('url=', url);
+    this.getStorageByName(name).subscribe(
+      (res: Response) => {
+        console.log("body=" + res['_body']);
+        this.http.delete(url, JSON.parse(res['_body'])).subscribe();
+      });
+
+  }
+
+
   createStorage(name: string, size: number, type: string) {
     let url = BASE_IAAS_URI + '/standalone_shares';
     let body = {
@@ -42,21 +94,21 @@ export class StorageService {
 
     let url = BASE_IAAS_URI + '/standalone_shares';
     let body = {
-        "tenant": {
-          "user": {
-            "id": "c774f0139d454df58aa20744832bb1c6",
-            "name": "admin",
-            "password": "qydcos"
-          },
-          "project": {
-            "id": "64ed158db85f4d1c8c28948d1ecfcdf5",
-            "name": "admin"
-          }
+      "tenant": {
+        "user": {
+          "id": "c774f0139d454df58aa20744832bb1c6",
+          "name": "admin",
+          "password": "qydcos"
         },
-        "share": {
-          "share_id": shareId
+        "project": {
+          "id": "64ed158db85f4d1c8c28948d1ecfcdf5",
+          "name": "admin"
         }
-      };
+      },
+      "share": {
+        "share_id": shareId
+      }
+    };
     console.log('url=', url);
     console.log('body=', JSON.stringify(body));
 
