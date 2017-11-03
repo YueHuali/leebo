9bc1a50b163feeaf7c1fca336c4119de82711048
fangming <1163443343@qq.com>
2017-11-03 09:53:16 +0800

    移除组织未调用iaas-api解除openstack对象项目和用户的关联

--- src/app/shared/services/organization.service.ts
+++ src/app/shared/services/organization.service.ts
@@ -84,6 +84,7 @@ export class OrganizationService {
   }
 
   leaveOrg(user: string, org: string): Observable<any> {
+    this.http.delete(BASE_IAAS_SERVICE + '/iaasUsers/' + user + '/organization/' + org).subscribe();
     let body =  {metadata : {labels: {}}};
     body.metadata.labels['org.' + org] = null;
     return this.http.patch(BASE_URI + '/pservice/users/' + user, body);
