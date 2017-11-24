47056b4b3fb96d6bf134470d437f9c43bfab128c
梅礼遇 <1126767991@qq.com>
2017-11-24 14:44:05 +0800

    #987【IaaS】导入画面追加按组织的过滤显示功能 --路由器部分

--- src/app/iaas/router/import-router/import-router.component.ts
+++ src/app/iaas/router/import-router/import-router.component.ts
@@ -1,21 +1,37 @@
 import {Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import {ResourceConfigService} from '../../../shared/services/resource-config.service';
 @Component({
   selector: 'import-router',
-  templateUrl: './import-router.component.html'
+  templateUrl: './import-router.component.html',
+  providers: [  ResourceConfigService ]
 })
 
 export class ImportRouterComponent implements OnInit {
   @Input() importRouterList: any;
   @Output() onSave = new EventEmitter();
   importRouterIds: any[] = [];
-
-  constructor() {
+  orgName:any;
+  projectList: any[];
+  constructor(private resourceConfigService: ResourceConfigService) {
   }
 
   ngOnInit() {
+    this.orgName='所有组织';
+    this.resourceConfigService.getIaasProjects().subscribe(
+      (data) => this.projectList = data.json()
+    );
   }
 
+  check(item:any){
+    if(this.orgName==='所有组织'){
+       return true;
+    }else {
+            if(this.orgName===item.projectName){
+                   return true;
+            }
+                  return false;
+             }
+    }
   checkRouterImport(event, id) {
     let checked = event.target.checked;
     if (checked === true) {
